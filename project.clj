b413d1d58e80d2d7ae3a4e85d1162aeea10e12e6
Takashi AOKI <federkasten@me.com>
2014-04-04 15:32:00 +0900

    bump up versions of dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject butler "0.1.0-SNAPSHOT"
   :description "A minimal ClojureScript library for Web Workers"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]]
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2202"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
   :profiles {:dev {:plugins [[com.cemerick/austin "0.1.4"]]}}
   :source-paths ["src"]
   :cljsbuild {:builds
